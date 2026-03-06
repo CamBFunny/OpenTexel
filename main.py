ab8a1ba6aa5ed3acca220ff1a3aaef51067621ae
CameronBFunny <156382194+CamBFunny@users.noreply.github.com>
2026-03-06 15:19:19 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+import time
+import numpy as np
+
+def Fighter(self, name, HP, ATK, DEF, WIS, AGI, LV, SEF, rarity, tribe, sign, type)
+    Fighter.name = name
+    Fighter.HP = HP
+    Fighter.ATK = ATK
+    Fighter.DEF = DEF
+    Fighter.WIS = WIS
+    Fighter.AGI = AGI
+    Fighter.LV = LV
+    Fighter.SEF = SEF
+    Fighter.rarity = rarity
+    Fighter.tribe = tribe
+    Fighter.sign = sign
+    Fighter.type = type
+
+empty = Fighter('-', 0, 0, 0, 0, 0, 0, 0, 'null', 'null', 'null', 'null')
+band = np.array([['empty',] * 3]*3)
+
+while running:
+  #game loop
+  pygame.display.flip()
